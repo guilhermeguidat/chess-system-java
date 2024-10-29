728d909895409ffa1cd0eaee40acaaa66a06e87e
Guilherme Guida <gguilhermecontato1@gmail.com>
2024-10-29 11:29:43 -0300

    Criação do chess layer e imprimindo o tabuleiro.

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,13 @@ package application;
 
 import boardgame.Position;
 import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
-        Board board = new Board(8, 8);
+        ChessMatch match = new ChessMatch();
+        UI.printBoard(match.getPieces());
     }
 }
