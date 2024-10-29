2aed5e4466697bf15a580ead4bbab6099ab7f9ad
Guilherme Guida <gguilhermecontato1@gmail.com>
2024-10-29 16:08:17 -0300

    Lançamento de exceção e alteração da posição da peça.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,8 +24,12 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char col, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(col, (char) row).toPosition());
+    }
+
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.WHITE), new Position(0, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.WHITE));
     }
 }
